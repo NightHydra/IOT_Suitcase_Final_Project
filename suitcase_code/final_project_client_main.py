da611e5a81414517d4455f6f4989593c63920016
Alek Krupka <alekmk02@gmail.com>
2025-04-14 19:04:45 -0400

    Added client code

---
+++ suitcase_code/final_project_client_main.py
@@ -0,0 +1,45 @@
+
+import paho.mqtt.client as mqtt
+import time as time
+
+CLIENT_ID = ""
+THE_BROKER = "192.168.199.1"
+BROKER_PORT = 8884
+
+class MQTTHandler:
+	def __init__(self):
+		self.client = mqtt.Client(client_id=CLIENT_ID, clean_session=True, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
+		self.client.on_publish = self.__on_publish
+		self.client.on_connect = self.__on_connect
+		
+		self.msg_num = 0
+		
+	def publish_image_to_server(self, msg):
+		self.client.publish("IOT_SUITCASE_DATA/IMAGES", qos=1, retain=False, payload = msg)
+		
+		
+	def connect_to_broker(self):
+		self.client.connect(THE_BROKER, port=BROKER_PORT, keepalive=60)
+		
+	def start_client(self):
+		
+		self.publish_image_to_server("Hello World")
+		
+		self.client.loop_forever()
+	
+	def __on_connect(self, client, userdata, flags, rc):
+		
+		print ("CONNECTED!!")
+		
+	def __on_publish(self, client, userdata, mid):
+		self.msg_num += 1
+		time.sleep(0.5)
+		self.publish_image_to_server("IM LOST "+str(self.msg_num))
+
+
+mqtt_handler = MQTTHandler()
+
+mqtt_handler.connect_to_broker()
+
+mqtt_handler.start_client()
+		
